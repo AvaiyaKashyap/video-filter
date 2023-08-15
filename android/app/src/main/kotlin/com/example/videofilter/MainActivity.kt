58803046e240dbe970a2b4e2bb8d6279bd49014f
Kashyap Avaiya <88942744+AvaiyaKashyap@users.noreply.github.com>
2023-08-15 20:56:41 +0530

    video with filter

---
+++ android/app/src/main/kotlin/com/example/videofilter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.videofilter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,207 @@
+import 'dart:io' show File;
+
+import 'package:path_provider/path_provider.dart';
+import 'package:flutter/material.dart' hide Rect;
+import 'package:flutter_gpu_video_filters/flutter_gpu_video_filters.dart';
+import 'package:flutter_gpu_filters_interface/flutter_gpu_filters_interface.dart';
+
+import 'approved_filters.dart';
+import 'filters.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Filters Demo',
+      theme: ThemeData(
+        useMaterial3: true,
+        primarySwatch: Colors.green,
+      ),
+      home: const ListPage(),
+    );
+  }
+}
+
+class ListPage extends StatelessWidget {
+  const ListPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Filters'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: CustomScrollView(
+          slivers: [
+            SliverFixedExtentList(
+              delegate: SliverChildBuilderDelegate(
+                (context, index) {
+                  final item = kFailedFilters[index];
+                  return Card(
+                    child: ListTile(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) {
+                              return FilterPage(
+                                configuration: item.configuration,
+                              );
+                            },
+                          ),
+                        );
+                      },
+                      title: Text(item.name),
+                      trailing: Icon(
+                        Icons.arrow_forward,
+                        color: Theme.of(context).colorScheme.error,
+                      ),
+                    ),
+                  );
+                },
+                childCount: kFailedFilters.length,
+              ),
+              itemExtent: 64,
+            ),
+            SliverFixedExtentList(
+              delegate: SliverChildBuilderDelegate(
+                (context, index) {
+                  final item = kFilters[index];
+                  return Card(
+                    child: ListTile(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) {
+                              return FilterPage(
+                                configuration: item.configuration,
+                              );
+                            },
+                          ),
+                        );
+                      },
+                      title: Text(item.name),
+                      trailing: Icon(
+                        Icons.arrow_forward,
+                        color: Theme.of(context).primaryColor,
+                      ),
+                    ),
+                  );
+                },
+                childCount: kFilters.length,
+              ),
+              itemExtent: 64,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class FilterPage extends StatefulWidget {
+  final GPUFilterConfiguration configuration;
+
+  const FilterPage({super.key, required this.configuration});
+
+  @override
+  State<FilterPage> createState() => _FilterPageState();
+}
+
+class _FilterPageState extends State<FilterPage> {
+  late final VideoPreviewController controller;
+  late final GPUVideoPreviewParams previewParams;
+  bool previewParamsReady = false;
+  static const _videoAsset = 'videos/demo.mp4';
+
+  @override
+  void initState() {
+    super.initState();
+    _prepare().whenComplete(() => setState(() {}));
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    widget.configuration.dispose();
+    super.dispose();
+  }
+
+  Future<void> _prepare() async {
+    await widget.configuration.prepare();
+    previewParams = await GPUVideoPreviewParams.create(widget.configuration);
+    previewParamsReady = true;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Preview'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        heroTag: null,
+        onPressed: () {
+          _exportVideo();
+        },
+        tooltip: 'Export video',
+        child: const Icon(Icons.save),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Container(
+          height: 200,
+          width: 300,
+          child: previewParamsReady
+              ? GPUVideoNativePreview(
+                  params: previewParams,
+                  configuration: widget.configuration,
+                  onViewCreated: (controller, outputSizeStream) async {
+                    this.controller = controller;
+                    await controller
+                        .setVideoSource(AssetInputSource(_videoAsset));
+                    await widget.configuration.update();
+                    await for (final _ in outputSizeStream) {
+                      setState(() {});
+                    }
+                  },
+                )
+              : const Center(
+                  child: CircularProgressIndicator(),
+                ),
+        ),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  Future<void> _exportVideo() async {
+    const asset = _videoAsset;
+    final root = await getTemporaryDirectory();
+    final output = File(
+      '${root.path}/${DateTime.now().millisecondsSinceEpoch}.${asset.split('.').last}',
+    );
+    final watch = Stopwatch();
+    watch.start();
+    final processStream = widget.configuration.exportVideoFile(
+      VideoExportConfig(
+        AssetInputSource(asset),
+        output,
+      ),
+    );
+    await for (final progress in processStream) {
+      debugPrint('Exporting file ${(progress * 100).toInt()}%');
+    }
+    debugPrint('Exporting file took ${watch.elapsedMilliseconds} milliseconds');
+    debugPrint('Exported: ${output.absolute}');
+  }
+}
